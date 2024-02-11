a75ac3826d22a2385927432d0ea54a6a05525672
manuelndeffo <manueljunior.ndeffonkamgue@ensae.fr>
2024-02-11 23:57:16 +0000

    update solver

--- swap_puzzle/main.py
+++ swap_puzzle/main.py
@@ -2,8 +2,6 @@ from grid import Grid
 from graph import Graph
 from solver import Solver
 
-
-
 # create and print a grid of 2 lines and 3 columns
 g = Grid(2, 3)
 print(g)
@@ -23,20 +21,42 @@ g.plot_grid()
 
 # The solution of [[3,5,6],[4,1,2]] with the Naive method
 
+print('****************')
+
 N = Solver(2, 3, [[3, 5, 6], [4, 1, 2]])
 print(N)
 N_sol = N.get_solution()
 print('To solve N we need to apply this different swaps:', N_sol)
 print('The numbers of swap to solve with the Naive solution are:', len(N_sol))
 
+print('***************')
+
 
 # Create the graph for [[3, 5, 6], [4, 1, 2]]
+
+print('*************')
 
 graph_N = N.graph_construct()
 bfs_N = graph_N.bfs((3, 5, 6, 4, 1, 2), (1, 2, 3, 4, 5, 6))
 print(bfs_N)
-print('Numbers of swap needed to solve the bfs algorithm are:', len(bfs_N))
+print('Numbers of swap needed to solve the bfs algorithm are:', len(bfs_N)-1)
+
+print('*****************')
 
 # with the new bfs
-new_bfs_N = graph_N.bfs((3, 5, 6, 4, 1, 2), (1, 2, 3, 4, 5, 6))
-print('The number of solution with the new bfs algorithm', len(new_bfs_N))
+print('*****************')
+
+new_bfs_N = graph_N.new_bfs((3, 5, 6, 4, 1, 2), (1, 2, 3, 4, 5, 6))
+print('The number of solution with the new bfs algorithm', len(new_bfs_N)-1)
+
+print('*****************')
+
+# The solution of N using the BFS algorithm is 
+
+print('*****************')
+N = Solver(2, 3, [[3, 5, 6], [4, 1, 2]])
+B_sol = N.bfs_solution()
+print('To solve N we need to apply this different swaps:', B_sol)
+print('The numbers of swap to solve with the Bfs solution are:', len(B_sol))
+
+print('*****************')
--- swap_puzzle/solver.py
+++ swap_puzzle/solver.py
@@ -1,7 +1,6 @@
 from grid import Grid
 from graph import Graph
 
-<<<<<<< HEAD
 
 class Solver(Grid):
     """
@@ -25,80 +24,12 @@ class Solver(Grid):
     
     def check_column(self, elt):
         return (self.position(elt)[1] == self.position_order(elt)[1])
-=======
-class Solver(Grid): 
-    
-    def __init__(self,m,n,initial_state = []):
-        """
-        Args:
-            m (int): number of lines 
-            n (int): number of columns
-            initial_state (list): list of integers
-        Returns : 
-            The matrix from the list of integers and the size of the matrix
-        """
-        super().__init__(m,n,initial_state)
-        self.final=[list(range(i*n+1, (i+1)*n+1)) for i in range(m)]
-
-    
-    def position(self,elt):
-        """
-        Args:
-            elt (int): element to find in the grid
-        Returns:
-            tuple: the position (i, j) of the element in the current state grid
-        """
-        for i,row in enumerate(self.state):
-            for j,value in enumerate (row):
-                if value==elt:
-                    return (i,j)
-
-    def position_order(self,elt):
-         """
-        Args:
-            elt (int): element to find in the grid
-        Returns:
-            tuple: the position (i, j) of the element in the final ordered grid
-        """
-        for i,row in enumerate(self.final):
-            for j,value in enumerate (row):
-                if value==elt:
-                    return (i,j)
-    
-    def check_column(self, elt):
-        """
-        Args:
-            elt (int): element to check
-        Returns:
-            bool: True if the element is in the correct column, False otherwise
-        """
-        return (self.position(elt)[1]==self.position_order(elt)[1])
->>>>>>> f828d241b77828e9afbe6f07ccfbcb9f4c2c9d8d
 
     def check_row(self, elt):
-<<<<<<< HEAD
         return (self.position(elt)[0] == self.position_order(elt)[0])
     
     def correct_position(self, elt):
         return (self.position(elt) == self.position_order(elt))
-=======
-        """
-        Args:
-            elt (int): element to check
-        Returns:
-        """
-        return (self.position(elt)[0]==self.position_order(elt)[0])
-
-    
-    def correct_position(self,elt):
-        """
-        Args:
-            elt (int): element to check
-        Returns:
-            True if the element is in the correct position, False otherwise
-        """
-        return(self.position(elt)==self.position_order(elt))
->>>>>>> f828d241b77828e9afbe6f07ccfbcb9f4c2c9d8d
 
     def get_solution(self):
         """
@@ -136,16 +67,31 @@ class Solver(Grid):
                         sol.append((cell, (cell[0]-1, cell[1]))) 
         return sol
 
-    
     def bfs_solution(self):
+        """" Returns the solution of the grid using the BFS algorithm
+             For example the solution of [[1,2],[4,3]] would be [(3,4)]
+        """
         g = self.graph_construct()
-        tuple_sol=g.bfs(tuple(flatten_grid(self.state)),tuple(flatten_grid(self.final)))
-        sol=[]
-        for i in range(1,len(tuple_sol)):
-            a_n=i-1
-            print('tes',self.n)
-            for j in range(0,self.n):
-                if (tuple_sol[i][j] != tuple_sol[a_n][j]):
+        tuple_sol = g.bfs(tuple(flatten_grid(self.state)), tuple(flatten_grid(self.final)))
+        sol = []
+        for i in range(1, len(tuple_sol)):
+            for j in range(self.n*self.m):
+                if (tuple_sol[i][j] != tuple_sol[i-1][j]):
+                    sol.append((tuple_sol[i][j], tuple_sol[i-1][j]))
+                    break
+        return sol
+
+
+    def new_bfs_solution(self):
+        """" Returns the solution of the grid using the new BFS algorithm
+             For example the solution of [[1,2],[4,3]] would be [(3,4)]
+        """
+        g = self.graph_construct()
+        tuple_sol = g.bfs(tuple(flatten_grid(self.state)), tuple(flatten_grid(self.final)))
+        sol = []
+        for i in range(1, len(tuple_sol)):
+            for j in range(self.n*self.m):
+                if (tuple_sol[i][j] != tuple_sol[i-1][j]):
                     sol.append((tuple_sol[i][j], tuple_sol[i-1][j]))
                     break
         return sol
